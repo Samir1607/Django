4434f3cab3b87451d1b5acfc0dc9955802a771ef
Samir Padekar <119828185+Samir1607@users.noreply.github.com>
2023-05-25 18:52:49 +0530

    Update settings.py
    again saving the file

--- SamEcommerce/settings.py
+++ SamEcommerce/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "SamEcommerce.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": os.path.join(BASE_DIR, "static/"),
+        "DIRS": [os.path.join(BASE_DIR, "static/")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, "static/")
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
